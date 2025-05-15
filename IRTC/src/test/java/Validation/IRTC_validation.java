1fe6037d16be0389c750bd290d21442e28bbbdce
gbhadage4 <bhadageganesh4@gmail.com>
2025-05-16 00:18:50 +0530

    Revert "for revert"
    This reverts commit 2f37095780b4c6ce8f08460205985de80a319e86.

--- IRTC/src/test/java/Validation/IRTC_validation.java
+++ IRTC/src/test/java/Validation/IRTC_validation.java
@@ -19,7 +19,6 @@ public class IRTC_validation extends Utils implements Locators {
 
 	public void searchTrain(String fromcity, String tocity,String Date , String classType,String BookingType ) throws InterruptedException {
 		 sendKeys(fromCity,fromcity);
-		 sendKeys(fromCity,fromcity);
 		 click(selectfirstcity);
 		 sendKeys(ToCity,tocity);
 		 click(selectfirstDistinationcity);
